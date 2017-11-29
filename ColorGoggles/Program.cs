1d11e85951cde4b34996b65c2bdec115eaf86dd7
Daniele Salvagni <daniele.salvagni@gmail.com>
2017-11-30 00:23:26 +0100

    Check for dll and DPI fix

--- ColorGoggles/Program.cs
+++ ColorGoggles/Program.cs
@@ -15,16 +15,30 @@ namespace ColorGoggles
         [STAThread]
         static void Main()
         {
-            String thisprocessname = Process.GetCurrentProcess().ProcessName;
 
+            // DPI Fix
+            if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
+
+            // Allow only one instance
+            String thisprocessname = Process.GetCurrentProcess().ProcessName;
             if (Process.GetProcesses().Count(p => p.ProcessName == thisprocessname) > 1)
                 return;
 
+            // Check for igfxDHLib
+            if (!System.IO.File.Exists("igfxDHLib.dll")) {
+                MessageBox.Show("Library `igfxDHLib.dll` not found. Please follow installation instructions in README.txt",
+                    "Library not found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
             Application.Run(new Form1());
 
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
     }
 }
