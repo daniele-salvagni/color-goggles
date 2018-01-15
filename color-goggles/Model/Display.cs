5eb418a0c0ae697c83123b784ea2394f07dc3a5b
Daniele Salvagni <daniele.salvagni@gmail.com>
2018-01-15 20:54:58 +0100

    Apply saturation to secondary devices too

--- color-goggles/Model/Display.cs
+++ color-goggles/Model/Display.cs
@@ -16,8 +16,8 @@ namespace color_goggles.Model
 
         private uint ulDevices = 0u;
         private uint ulDeviceID = 0u;
+        private uint[] ulDeviceIDs = new uint[] { 0, 0, 0 };
         
-
         public Display(String deviceName) {
             DeviceName = deviceName;
         }
@@ -25,10 +25,10 @@ namespace color_goggles.Model
         public String DeviceName { get; }
 
         private void GetColorInfo() {
-
             IgfxDH.DataHandler.get_CurrentConfig(ref selectedConfig);
-            for (uint i = 0u; i < 3u; i++) {
+            for (uint i = 0; i < 3; i++) {
                 ulDevices |= selectedConfig.systemTopology.pathInfo[i].dwTargetId;
+                ulDeviceIDs[i] = selectedConfig.systemTopology.pathInfo[i].dwTargetId;
             }
             ulDeviceID = selectedConfig.systemTopology.pathInfo[0].dwTargetId;
 
@@ -38,33 +38,27 @@ namespace color_goggles.Model
             //uint[] array = new uint[3];
             //IgfxDH.DataHandler.get_GetDeviceList(colorDevices.ulDevices, array);
 
-            for (uint i = 0u; i < 3u; i++) {
+            for (uint i = 0; i < 3; i++) {
                 colorDevices.Device[i] = default(_CUI_COLOR_INFO);
             }
 
             colorDevices.Command = _CUI_COLOR_COMMAND.GET_COLOR;
             IgfxDH.DataHandler.get_color(0u, ref colorDevices);
-
         }
 
         public int GetSaturation() {
-
             GetColorInfo();
             return (int)colorDevices.Device[0].Saturation.fCurrent;
-
         }
 
         public void SetSaturation(int satValue) {
-
             GetColorInfo();
-
-            for (uint num = 0u; num < 3u; num += 1u) {
-                colorDevices.Device[num].Saturation.fCurrent = satValue; 
-            }
-
             colorDevices.Command = _CUI_COLOR_COMMAND.CHANGE_DEVICE_COLOR;
-            IgfxDH.DataHandler.set_color(ulDeviceID, ref colorDevices, 0u);
 
+            for (uint num = 0; num < 3; num += 1) {
+                colorDevices.Device[num].Saturation.fCurrent = satValue;
+                IgfxDH.DataHandler.set_color(ulDeviceIDs[num], ref colorDevices, 0u);
+            }
         }
 
 
